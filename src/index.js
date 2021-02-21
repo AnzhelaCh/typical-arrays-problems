4be8b62e3595c73302b2ffde6978a4a3b73a136a
Chernenko Anzhela <chelsea@templatemonster.me>
2021-02-21 17:28:35 +0200

    feat: add functions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,31 @@
-
 exports.min = function min (array) {
-  return 0;
+    if(!array || !array.length){
+        return 0;
+    } else {
+        return Math.min.apply(null, array);
+    }
+
 }
 
 exports.max = function max (array) {
-  return 0;
+    if(!array || !array.length){
+        return 0;
+    } else {
+        return Math.max.apply(null, array);
+    }
+
 }
 
 exports.avg = function avg (array) {
-  return 0;
+    if(!array || !array.length){
+        return 0;
+    }else {
+        var sum = 0;
+        var count = array.length;
+        for (let i=0; i<count; i++) {
+            sum += array[i];
+        }
+        return sum/count;
+    }
+
 }
